362983c90dc79289d73f68c98ca06ddf449fb4b2
Jeff C <jeffplv@gmail.com>
2024-04-10 16:51:56 -0400

    working dashboard

--- server/routes/top10.js
+++ server/routes/top10.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const axios = require('axios');
 require('dotenv').config();
-const dashBoardController = require('../controllers/dashBoard')
+const dashBoardController = require('../controllers/dashBoard');
 
 const router = express.Router();
 const jobController = require('../controllers/jobController');
 const gptController = require('../controllers/gptController');
 
+// router.post('/', jobController.top10, gptController.compare, dashBoardController.job, (req, res) => {
+router.get(
+  '/',
+  jobController.top10,
 
-router.post('/', jobController.top10, gptController.compare, dashBoardController.job, (req, res) => {
-  // const responseData = {
-  //   data: res.locals.data,
-  //   gpt: res.locals.gpt
-  // }
-  // return res.status(200).json(responseData);
+  (req, res) => {
+    // const responseData = {
+    //   data: res.locals.data,
+    //   gpt: res.locals.gpt
+    // }
+    // return res.status(200).json(responseData);
 
-  return res.status(200).json(res.locals.table)
-});
+    // return res.status(200).json(res.locals.table);
+    console.log(res.locals.data);
+    return res.status(200).json(res.locals.data);
+  }
+);
 
 module.exports = router;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/DashBoard.css';
-// const abc = require('../../server/routes/top10')
+
 export const DashBoardPage = () => {
   const [jobListings, setJobListings] = useState([]);
 

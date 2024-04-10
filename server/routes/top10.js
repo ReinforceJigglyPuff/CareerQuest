6e6dfcf89f2ab707b026a544baecce1c86cc66bd
Jimmy Ho <jimmy@Jimmys-MacBook-Pro.local>
2024-04-10 13:26:39 -0400

    fixed return statement for top10

--- server/routes/top10.js
+++ server/routes/top10.js
@@ -4,7 +4,11 @@ const jobController = require('../controllers/jobController');
 const gptController = require('../controllers/gptController');
 
 router.post('/', jobController.top10, gptController.compare, (req, res) => {
-  return res.status(200).json(res.locals.data).json(res.locals.gpt);
+  const responseData = {
+    data: res.locals.data,
+    gpt: res.locals.gpt
+  }
+  return res.status(200).json(responseData);
 });
 
 module.exports = router;
